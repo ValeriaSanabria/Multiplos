7c6cbab85ca85b51a9865d3bbcfe2ebaf7316943
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-18 19:39:47 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-rotulo1.innerHTML = "Indique el un numero";
-rotulo2.innerHTML = "Indique el numero multiplo";
-
-let numero1: number = Number(dato1.value);
-let numero2: number = Number(dato2.value);
+let numero1: number = 2;
+let numero2: number = 2;
 
 function esMultiplo(numero1: number, numero2: number): boolean {
   return numero1 % numero2 === 0;
 }
 
 if (esMultiplo(numero1, numero2)) {
-  console.log(numero1, "Es multiplo de ", numero2);
+  console.log(numero1 + " Es multiplo de " + numero2);
 } else {
-  console.log(numero1, "No es multiplo de ", numero2);
+  console.log(numero1 + " No es multiplo de " + numero2);
 }
